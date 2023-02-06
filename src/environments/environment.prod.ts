95e1b8b460a1e4194f4b9c4547ee366b2e107441
cesar-chembi <c.chembi@uniandes.edu.co>
2023-02-05 22:46:30 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,3 +6,4 @@ export const environment = {
   urlPaciente: `${host}api/v1/pacientes/`,
   urlEspecialidad: `${host}api/v1/especialiades/`,
   urlEspecialidadMedico: `${host}api/v1/medicosespecialidad/`
+}
