f8f515f92cdd1a35ef75d224f1f301a3d9e8139f
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-02-05 21:57:35 -0500

    HU01-Finaliza el desarrollo de la historia.

--- src/app/registro/medico.service.spec.ts
+++ src/app/registro/medico.service.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClient, HttpHandler} from "@angular/common/http";
 import { MedicoService } from './medico.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('MedicoService', () => {
   let service: MedicoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [MedicoService]
+    });
     service = TestBed.inject(MedicoService);
+    httpMock = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
